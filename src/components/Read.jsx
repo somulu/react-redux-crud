9bb16446436d682a0b4f2c723aeb4b37cea54d3d
gugulothusomulu06@gmail.com <gugulothusomulu06@gmail.com>
2024-02-19 23:35:26 +0530

    first commit

---
+++ src/components/Read.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { deleteUser, showUser } from "../features/userDetailSlice";
+import CustomModal from "./CustomModal";
+
+const Read = () => {
+  const dispatch = useDispatch();
+
+  const [id, setId] = useState();
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const { users, loading, searchData } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(showUser());
+  }, [dispatch]);
+
+  if (loading) {
+    return <h2>Loading</h2>;
+  }
+
+  return (
+    <div>
+      {showPopup && (
+        <CustomModal
+          id={id}
+          showPopup={showPopup}
+          setShowPopup={setShowPopup}
+        />
+      )}
+      <h2>All data</h2>
+
+      <div>
+        {users &&
+          users
+            .filter((ele) => {
+              if (searchData.length === 0) {
+                return ele;
+              } else {
+                return ele.name
+                  .toLowerCase()
+                  .includes(searchData.toLowerCase());
+              }
+            })
+            .map((ele) => (
+              <div key={ele.id} className="card w-50 mx-auto my-2">
+                <div className="card-body">
+                  <h5 className="card-title">{ele.name}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">{ele.email}</h6>
+                  <p className="card-text">{ele.gender}</p>
+                  <button
+                    className="card-link"
+                    onClick={() => [setId(ele.id), setShowPopup(true)]}
+                  >
+                    View
+                  </button>
+                  <Link to={`/edit/${ele.id}`} className="card-link">
+                    Edit
+                  </Link>
+                  <Link
+                    onClick={() => dispatch(deleteUser(ele.id))}
+                    className="card-link"
+                  >
+                    Delete
+                  </Link>
+                </div>
+              </div>
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default Read;
